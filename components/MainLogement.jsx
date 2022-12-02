2c949170dcf51943bf1e0464f60d6a8b6f7862da
ADAM ESHAG <adameshag93@gmail.com>
2022-12-02 16:59:54 +0100

    Add informations div with style

--- components/MainLogement.jsx
+++ components/MainLogement.jsx
@@ -1,8 +1,21 @@
 //-----MainLogement----//
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom'
 import { useSearchParams } from "react-router-dom";
+import { useState } from 'react';
+
+//----Import Carousel From React-----//
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader  
 import { Carousel } from 'react-responsive-carousel';
+
+//---Import React-Chevron---//
+import Chevron from 'react-chevron'
+
+//----Import FontAwesomeIcon from React---//
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+
 
 export default function MainLogement(props) {
 
@@ -25,11 +38,78 @@ export default function MainLogement(props) {
             )
         })
         
+        /*** Maper tags****/
+        const tags = props.item.tags;
+
+        const tag = tags.map(item => {
+            return (
+                <span key={item} className='tag'>{item}</span>
+            )
+        })
+
+        /**** Maper éqipments ****/
+        const equipments= props.item.equipments
+        const equipment = equipments.map(item => {
+            return (
+                <li key={item}>{item}</li>
+            )
+        })
+
+        /***-----Function showDescription------***/
+        // function ShowDescription() {
+        //     const [showParagraphe, setShowParagraphe] = React.useState(true);
+        //     const onClick = () => setShowParagraphe(false);
+        //     const Show = <div>
+        //         <div className='chevron'>
+        //             <p>Description</p> <Chevron direction={'down'} />
+        //         </div>
+        //         <p>{props.item.description}</p>
+        //     </div>;
+        //     return (
+        //         <div>
+        //             {showParagraphe ? <Show /> : null}
+        //         </div>
+        //     );
+        // }
+        
         return (
                 <>
                     <Carousel>
                        {image}
                     </Carousel>
+                    <div className='informations'>
+                        <div className='titre'>
+                            <h2>{props.item.title}</h2>
+                            <p>{props.item.location}</p>
+                            <div className='tags'>
+                                {tag}
+                            </div>
+                        </div>
+                        <div className='host'>
+                            <div className='host--stars'>
+                                <span><FontAwesomeIcon icon={faStar} /></span>
+                                <span><FontAwesomeIcon icon={faStar} /></span>
+                                <span><FontAwesomeIcon icon={faStar} /></span>
+                                <span><FontAwesomeIcon icon={faStar} /></span>
+                                <span><FontAwesomeIcon icon={faStar} /></span>
+                            </div>
+                            <div className='host--name'>
+                                <h6>{props.item.host.name}</h6>
+                                <img src={props.item.host.picture} alt="Hot-Picture" />
+                            </div>
+                        </div>
+                        <div className='description--equipments'>
+                            <div className="description">
+                                {/* <ShowDescription /> */}
+                            </div>
+                            
+                            <div className='equipments'>
+                                Equipements <Chevron direction={'up'}/>
+                                <ul>{equipment}</ul>
+                            </div>
+                        </div>
+
+                    </div>
                 </>
             )
     }    
